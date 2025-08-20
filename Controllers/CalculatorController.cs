b62d918f310275ff454cb6f46baedfd4deba4014
Ana Camila Lopez Correa <a2024112021@estudiantes.upsa.edu.bo>
2025-08-20 19:52:58 -0400

    Segundo Commit

--- Controllers/CalculatorController.cs
+++ Controllers/CalculatorController.cs
@@ -1,4 +1,4 @@
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;   // Necesario para ControllerBase, IActionResult, Route, ApiController, FromQuery
 
 namespace CalculatorApp1.Controllers
 {
@@ -6,38 +6,10 @@ namespace CalculatorApp1.Controllers
     [ApiController]
     public class CalculatorController : ControllerBase
     {
-        // GET: api/calculator/sum?a=5&b=3
         [HttpGet("sum")]
         public IActionResult GetSum([FromQuery] int a, [FromQuery] int b)
         {
-            int result = a + b;
-            return Ok(new { a, b, result });
-        }
-
-        // GET: api/calculator/subtract?a=5&b=3
-        [HttpGet("subtract")]
-        public IActionResult GetSubtract([FromQuery] int a, [FromQuery] int b)
-        {
-            int result = a - b;
-            return Ok(new { a, b, result });
-        }
-
-        // GET: api/calculator/multiply?a=5&b=3
-        [HttpGet("multiply")]
-        public IActionResult GetMultiply([FromQuery] int a, [FromQuery] int b)
-        {
-            int result = a * b;
-            return Ok(new { a, b, result });
-        }
-
-        // GET: api/calculator/divide?a=6&b=3
-        [HttpGet("divide")]
-        public IActionResult GetDivide([FromQuery] int a, [FromQuery] int b)
-        {
-            if (b == 0)
-                return BadRequest("No se puede dividir entre cero.");
-
-            double result = (double)a / b;
+            var result = a + b;
             return Ok(new { a, b, result });
         }
     }
